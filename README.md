da2f160bb3c37f8dcf2846defaf056da8062aad6
Jasmin Rubinovitz <jasrub@gmail.com>
2017-01-05 22:48:57 -0800

    add smiles

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # IDEO-pop-quiz
 
 "a quick little something your teenage self would think is totally awesome."
+
+### Creatures Generator
+This is a simple creatures generator.
+Built using paper.js
+
+As a teenager, I loved sketching imaginary friend all the time.
+I built a fun little app that creates those creatures, and lets the user move them around, delete them, or export the result as an SVG file.
+
+![](creatures.png)
+
+
+This is live at [www.jasrub.com/IDEO-pop-quiz](www.jasrub.com/IDEO-pop-quiz)
--- js/myScript.js
+++ js/myScript.js
@@ -5,7 +5,7 @@
 var values = {
     paths: 30,
     minPoints: 5,
-    maxPoints: 18,
+    maxPoints: 22,
     minRadius: 80,
     maxRadius: 200
 };
@@ -46,6 +46,7 @@ function createBlob(center, maxRadius, points) {
     var group = new Group();
     group.addChild(path);
     group.addChildren(createEyes(center, maxRadius));
+    group.addChild(createMouth(center, maxRadius));
 
     // When the mouse is double clicked on the item, remove it:
     group.onDoubleClick = function(event) {
@@ -58,22 +59,48 @@ function createBlob(center, maxRadius, points) {
     }
 }
 
+function createMouth(center, maxRadius) {
+
+    var mouth = new Path();
+    mouth.strokeColor = 'black';
+
+    var length = 30;//0.5 * (minRadius + maxRadius);
+
+    var pts = 3 + Math.random() * 5;
+
+    mouthOffset = new Point(0, Math.random() * 30);
+
+    for (var i = 0; i < pts; i++) {
+        var r = Math.random() * 10;
+        mouth.add(center + mouthOffset + new Point({ length: length + r, angle: 180 / pts * i }));
+    }
+
+    mouth.smooth();
+    return mouth;
+
+}
+
 function createEyes(center, maxRadius){
     var eyeMinSize = 3;
-    var eyeMaxSize = 15
-    var eyeOffset = (Math.random() * maxRadius * 0.5)
+    var eyeMaxSize = 15;
+    var r1 = getRandomArbitrary(eyeMinSize, eyeMaxSize);
+    var r2 = getRandomArbitrary(eyeMinSize, eyeMaxSize);
+    var eyesColor = new Color(Math.random());
+    var eyeOffset = getRandomArbitrary((r1+r2)/2, maxRadius * 0.5)
     var eye1 = new Path.Circle({
-        radius: getRandomArbitrary(eyeMinSize, eyeMaxSize),
+        radius: r1,
         center: [center.x+eyeOffset, center.y],
-        strokeColor: 'black'
+        strokeColor: 'black',
+        fillColor: eyesColor
+
     });
     var eye2 = new Path.Circle({
-        radius: getRandomArbitrary(eyeMinSize, eyeMaxSize),
+        radius: r2,
         center: [center.x-eyeOffset, center.y],
-        strokeColor: 'black'
+        strokeColor: 'black',
+        fillColor:eyesColor
     });
     return [eye1, eye2]
-
 }
 
 
@@ -88,4 +115,28 @@ function onMouseDown(event) {
 // Returns a random number between min (inclusive) and max (exclusive)
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+//currently name doesn't seem to work in some browsers.
+//Save SVG from paper.js as a file.
+var downloadAsSVG = function (fileName) {
+
+    if(!fileName) {
+        fileName = "creature_generator.svg"
+    }
+
+    var url = "data:image/svg+xml;utf8," + encodeURIComponent(project.exportSVG({asString:true}));
+
+    var link = document.createElement("a");
+    link.download = fileName;
+    link.href = url;
+    link.click();
+}
+
+
+//Listen for SHIFT-P to save content as SVG file.
+function onKeyUp(event) {
+    if(event.character == "P") {
+        downloadAsSVG();
+    }
 }
